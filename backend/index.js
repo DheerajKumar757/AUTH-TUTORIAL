27f4c0d0d52d93b8caf8f7cb20bbadf093772d88
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2025-11-03 10:13:57 +0530

    C3 : Basic script and express setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello, World! 1234");
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
+
+
+// rXxYnCZlCOKGgK9q
