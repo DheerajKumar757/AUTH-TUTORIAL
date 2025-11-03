1ec9c8cab2b0bfc6743d43dd041f33f961186b8f
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2025-11-03 12:31:42 +0530

    C8 : Implement inital signup
    Add signup functionality get user data hash the
    password and save the data to database.

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,34 @@
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
 export const signup = async (req, res) => {
-    res.send("signup route");
+    const { email, password, name } = req.body;
+    try {
+        if(!email || !password || !name) {
+            throw new Error("All fields are required");
+        }
+
+        const userAlreadyExists = await User.findOne({email});
+        if(userAlreadyExists) {
+            return res.status(400).json({success:false, message: "User already exists"});
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 12);
+        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+
+        const user = new User({
+            email,
+            password: hashedPassword,
+            name,
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000 // 24 hours
+        })
+
+        await user.save();
+    }
+    catch (error) {
+        return res.status(400).json({success:false, message: error.message});
+    }
 }
 
 export const login = async (req, res) => {
