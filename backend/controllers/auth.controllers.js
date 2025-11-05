06d1ceac5180adadc41f0f9be5569f039e79984a
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2025-11-05 16:58:38 +0530

    C15 : Impl login API

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -85,7 +85,35 @@ export const verifyEmail = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    res.send("login route");
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email});
+        if(!user) {
+            return res.status(400).json({ success: false, message: "User not registered."});
+        }
+        const isPasswordValid = await bcryptjs.compare(password, user.password);
+        if(!isPasswordValid) {
+            return res.status(400).json({ success: false, message: "Password is incorrect."});
+        }
+
+        generateTokenAndSetCookie(res, user._id);
+
+        user.lastLogin = new Date();
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Logged in successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            },
+        });
+    }
+    catch {
+        console.log("Error in login. ", error);
+        return res.status(400).json({ success: false, message: error.message});
+    }
 }
 
 export const logout = async (req, res) => {
