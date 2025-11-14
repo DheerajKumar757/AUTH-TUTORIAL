7b1f25d83c8fc22d085f0de31fe56f6aa4427be5
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2025-11-14 20:30:30 +0530

    C30 : Add Zustand store and integrate signup with API
    Install axios and zustand packages, create authStore for global state management, and update SignUpPage to use store for signup functionality

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
-import { Lock, Mail, User } from 'lucide-react'
+import { Loader, Lock, Mail, User } from 'lucide-react'
 import Input from '../components/Input';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import PasswordStrengthMeter from '../components/PasswordStrengthMeter';
+import { useAuthStore } from '../store/authStore';
 
 const SignUpPage = () => {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const { signup, error, isLoading } = useAuthStore();
 
-    const handleSignUp = (e) => {
+    const handleSignUp = async (e) => {
         e.preventDefault();
+
+        try {
+            await signup(email, password, name);
+            navigate('/verify-email');
+        } 
+        catch (error) {
+            console.log(error);
+        }
     };
   return (
     <motion.div
@@ -49,15 +60,16 @@ const SignUpPage = () => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
-
+                {error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
                 <PasswordStrengthMeter password={password} />
 
                 <motion.button className='mt-5 w-full py-3 px-4 bg-linear-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200'
                 whileHover={{ scale: 1.02 }}
                 whileTap={{ scale: 0.98 }}
                 type='submit'
+                disabled={isLoading}
                 >
-                    Sign Up
+                    {isLoading ? <Loader className='animate-spin mx-auto' size={24} /> : "Sign Up"}
                 </motion.button>
 
             </form>
