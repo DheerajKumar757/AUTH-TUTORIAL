bb38791c3dd4df3d5f53a1e1f507d3c20886a4fe
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2025-11-16 03:10:19 +0530

    C34 : Add ProtectedRoute and integrate DashboardPage for authenticated users
    Introduced a new ProtectedRoute component to secure routes requiring authentication and verified users.
    Implemented conditional redirects for unauthenticated users (to /login) and unverified users (to /verify-email).
    Integrated DashboardPage as the main component for the root route / wrapped inside ProtectedRoute.
    Updated routing logic in App.jsx to enforce authentication flow and improve user session management.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,22 @@ import EmailVerificationPage from "./pages/EmailVerificationPage"
 import { Toaster } from "react-hot-toast"
 import { useAuthStore } from "./store/authStore"
 import { useEffect } from "react"
+import DashboardPage from "./pages/DashboardPage"
 
+// protect routes that require authentication
+const ProtectedRoute = ({children}) => {
+  const {isAuthenticated, user} = useAuthStore();
+
+  if(!isAuthenticated) {
+    return <Navigate to="/login" replace />
+  }
+
+  if(!user.isVerified) {
+    return <Navigate to="verify-email" replace />
+  }
+
+  return children;
+}
 
 // redirect authenticated users to the home page
 const RedirectAuthenticatedUser = ({children}) => {
@@ -36,7 +51,11 @@ function App() {
       <FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
 
       <Routes>
-        <Route path='/' element={"Home"} />
+        <Route path='/' element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        } />
         <Route path='/signup' element={
           <RedirectAuthenticatedUser>
             <SignUpPage />
